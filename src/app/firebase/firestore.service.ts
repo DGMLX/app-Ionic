8968979f78b7db7841a4a3fdca2c0d0dae20e122
Vina del Mar <vina@duoc.acad>
2024-10-21 11:59:29 -0300

    cambiando horas

--- src/app/firebase/firestore.service.ts
+++ src/app/firebase/firestore.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { initializeApp } from '@angular/fire/app';
+import { Firestore, getFirestore } from '@angular/fire/firestore';
 import { collection, addDoc } from "firebase/firestore"; 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,8 +20,17 @@ export class FirestoreService {
    //CRUD
   
    async CrearUsuario(){
-    console.log('Creando Usuario')
 
+    // try {
+    //   const docRef = await addDoc(collection(this.firestore, "users"), {
+    //     first: "Ada",
+    //     last: "Lovelace",
+    //     born: 1815
+    //   });
+    //   console.log("Document written with ID: ", docRef.id);
+    // } catch (e) {
+    //   console.error("Error adding document: ", e);
+    // }
    }
 
    async login(){
