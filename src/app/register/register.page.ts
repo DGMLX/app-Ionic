760b057677a4abd548d2c2d9730dd439fa372cce
Vina del Mar <vina@duoc.acad>
2024-11-06 12:16:36 -0300

    subiendo archivos con autentificacion creacion del manager y sesionService

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -16,4 +16,78 @@ export class RegisterPage implements OnInit {
   onLoginButtonPressed(){
     this.router.navigate(["/login"])
   }
+
+//   async onRegisterButtonPressed() {
+//     try {
+//       const userCredential = await this.sessionManager.registerUserWith(
+//         this.email,
+//         this.password
+//       );
+
+//       const user = userCredential.user;
+
+//       if (user) {
+//         this.alert.showAlert(
+//           'Registro exitoso',                         
+//           'Ya eres parte de nuestro sistema', 
+//           () => {    
+//             this.router.navigate(['/splash']);     
+//           }
+//         )
+//       } else {
+//         alert('¡Registro exitoso!');
+//       }
+
+      
+//       this.router.navigate(['/splash']);
+
+//     } catch (error: any) {
+
+//       switch (error.code) {
+//         case 'auth/email-already-in-use':
+//           this.alert.showAlert(
+//             'Error',                         
+//             'Este correo electrónico ya está en uso. Por favor, utiliza otro o inicia sesión.', 
+//             () => {    
+//               this.clean     
+//             }
+//           )
+//           break
+//         case 'auth/invalid-email':
+//           this.alert.showAlert(
+//             'Error',                         
+//             'La dirección de correo electrónico no es válida.', 
+//             () => {    
+//               this.clean     
+//             }
+//           )
+//           break
+//         case 'auth/weak-password':
+//           this.alert.showAlert(
+//             'Error',                         
+//             'La contraseña es muy débil.', 
+//             () => {    
+//               this.clean     
+//             }
+//           )
+//           break
+//         default:
+//           this.alert.showAlert(
+//             'Error',                         
+//             'Ocurrió un error al registrar el usuario: ' + error.message, 
+//             () => {    
+//               this.clean     
+//             }
+//           )
+//           break
+//       }
+//     }
+//   }
+
+//   clean() {
+//     this.email = ''
+//     this.password = ''
+//   }
+  
+// }
 }
---
+++ src/managers/sessionManager.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+//imports firebase
+import firebase from 'firebase/compat';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class SessionManager {
+
+    //agrego constructor
+    constructor(public fireAuth: AngularFireAuth) { }
+
+
+    private readonly temporaryUserName: string = 'user';
+    private readonly temporaryPass: string = 'pass';
+
+    performLogin(user: string, password: string): boolean {
+        if(user == this.temporaryUserName && password == this.temporaryPass) {
+            return true;
+        } else {
+            return false;
+        }  
+    }
+
+    async signOut() {
+        return await this.fireAuth.signOut()
+    }
+
+    async registerUserWith(email: string, password: string) : Promise<any> {
+        return await this.fireAuth.createUserWithEmailAndPassword(email, password)
+    }
+
+    async loginWith(email: string, password: string) : Promise<any> {
+        return await this.fireAuth.signInWithEmailAndPassword(email, password)
+    }
+    
+    async getProfile() {
+        return await this.fireAuth.currentUser
+    }
+}
